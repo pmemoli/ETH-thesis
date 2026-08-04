e8666a43dc5aa6d5fab8587b3c94a4f0c1cd29f6
pmemoli <pedromemoli@hotmail.com>
2026-08-04 12:19:37 +0200

    Removed hardware type for the profile

--- profile.py
+++ profile.py
@@ -7,16 +7,13 @@ import geni.portal as portal
 import geni.rspec.pg as rspec
 
 DEFAULT_IMAGE = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU24-64-STD"
-HARDWARE_TYPE = "r6525"
 
 # Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
 
 # Create a raw PC
 node = request.RawPC("node")
-
 node.disk_image = DEFAULT_IMAGE
-node.hardware_type = HARDWARE_TYPE
 
 # Write the request in RSpec format
 portal.context.printRequestRSpec()
