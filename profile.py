e9b5a701a462503e59bbf2fa718a9a96720ff821
pmemoli <pedromemoli@hotmail.com>
2026-08-07 14:44:49 +0200

    x

--- profile.py
+++ profile.py
@@ -10,16 +10,23 @@ DEFAULT_IMAGE = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU24-64-STD"
 
 # Define build-time params
 pc = portal.context
-
-pc.defineParameter(
-    "phystype",
-    "Hardware type",
-    portal.ParameterType.NODETYPE,
-    "",
-    longDescription="Leave blank to let the mapper pick whatever is free.",
-)
-
-params = pc.bindParameters()
+#
+# pc.defineParameter(
+#     "phystype",
+#     "Hardware type",
+#     portal.ParameterType.NODETYPE,
+#     "",
+#     legalValues=[
+#         ("", "Any available type"),
+#         ("m510", "Utah, Intel Xeon-D, 8 cores"),
+#         ("c6525-25g", "Utah, AMD EPYC Rome, 16 cores"),
+#         ("c220g5", "Wisconsin, Skylake, 20 cores"),
+#         ("c6420", "Clemson, Skylake, 32 cores"),
+#     ],
+#     longDescription="Leave blank to let the mapper pick whatever is free.",
+# )
+#
+# params = pc.bindParameters()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
